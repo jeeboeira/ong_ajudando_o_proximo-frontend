016ecec6cf9a8690e898d460374922bb730395a4
Jessé Boeira - Alienware <jee_boeira@hotmail.com>
2025-04-26 14:55:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ong_ajudando_o_proximo-frontend',
+  webDir: 'www'
+};
+
+export default config;
