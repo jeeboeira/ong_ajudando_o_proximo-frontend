d3ecb07c4e4a8c799c3316b74b601355024d6c6d
Jessé Boeira - Alienware <jee_boeira@hotmail.com>
2025-04-27 14:01:42 -0300

    html e ts do formulário pronto

--- src/app/modals/add-volunteer-modal/add-volunteer-modal.component.ts
+++ src/app/modals/add-volunteer-modal/add-volunteer-modal.component.ts
@@ -1,14 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular/standalone';
+import { IonTextarea, IonLabel, IonInput, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { VolunteerService } from 'src/app/services/volunteer.service';
 
 @Component({
   selector: 'app-add-volunteer-modal',
   templateUrl: './add-volunteer-modal.component.html',
   styleUrls: ['./add-volunteer-modal.component.scss'],
+  standalone: true,
+  imports: [IonTextarea, IonLabel, IonInput, IonItem, IonList, IonHeader,
+            IonToolbar, IonTitle, IonButtons, IonButton]
 })
-export class AddVolunteerModalComponent  implements OnInit {
+export class AddVolunteerModalComponent {
+  name: string = '';
+  email: string = '';
+  interestArea: string = '';
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController,
+    private volunteerService: VolunteerService
+  ) { }
 
-  ngOnInit() {}
+  // Método para fechar o modal
+  closeModal() {
+    this.modalCtrl.dismiss();
+  }
 
+    // Função para adicionar voluntário
+    addVolunteer() {
+      this.volunteerService.addVolunteer(this.name, this.email, this.interestArea).subscribe(
+        response => {
+          console.log('Voluntário adicionado com sucesso', response);
+          this.name = '';
+          this.email = '';
+          this.interestArea = '';
+        },
+        error => {
+          console.error('Erro ao adicionar voluntário', error);
+        }
+      )
+    }
+
+    onInputName(event: any) {
+      this.name = event.target.value || '';
+    }
+    onInputEmail(event: any) {
+      this.email = event.target.value || '';
+    }
+    onInputInterestArea(event: any) {
+      this.interestArea = event.target.value || '';
+    }
+            
 }
--- src/app/pages/volunteers/volunteers.page.ts
+++ src/app/pages/volunteers/volunteers.page.ts
@@ -2,28 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonIcon, IonBackButton, IonButton, IonButtons, IonContent, IonTitle, IonToolbar, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
-import { VolunteerService } from 'src/app/services/volunteer.service';
-import { Storage } from '@ionic/storage-angular';
 import { AddVolunteerModalComponent } from 'src/app/modals/add-volunteer-modal/add-volunteer-modal.component';
 import { ModalController } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { VolunteerService } from 'src/app/services/volunteer.service';
+
+
 
 @Component({
   selector: 'app-volunteers',
   templateUrl: './volunteers.page.html',
   styleUrls: ['./volunteers.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonBackButton, IonButtons, IonContent, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonButton]
+  imports: [IonIcon, IonBackButton, IonButtons, IonContent, IonTitle,
+            IonToolbar, CommonModule, FormsModule, IonList, IonItem,
+            IonLabel, IonButton]
 })
 export class VolunteersPage implements OnInit {
   volunteers: any[] = [];
-  name: string = '';
-  email: string = '';
-  interestArea: string = '';
 
   constructor(
-    private volunteerService: VolunteerService,
+    private modalCtrl: ModalController,
     private storage: Storage,
-    private modalCtrl: ModalController
+    private volunteerService: VolunteerService
+
   ) { }
 
   async ngOnInit() {
@@ -50,20 +52,4 @@ export class VolunteersPage implements OnInit {
     });
     return await modal.present();
   }
-
-  // Função para adicionar voluntário
-  addVolunteer() {
-    this.volunteerService.addVolunteer(this.name, this.email, this.interestArea).subscribe(
-      response => {
-        console.log('Voluntário adicionado com sucesso', response);
-        this.name = '';
-        this.email = '';
-        this.interestArea = '';
-      },
-      error => {
-        console.error('Erro ao adicionar voluntário', error);
-      }
-    )
-  }
-
 }
